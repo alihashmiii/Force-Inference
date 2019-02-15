6921091122c131b1ce22b5095769173ba81949a9
Ali Hashmi <alihashmi_87@hotmail.com>
2019-02-15 05:25:54 +0100

    Update Force Inference.m

--- Force Inference.m
+++ Force Inference.m
@@ -36,13 +36,15 @@ ReplacePixelValue[image,posConv-> 1]
 
 Options[associateVertices]= {"stringentCheck"-> True};
 associateVertices[img_,segt_,OptionsPattern[]]:= With[{dim =Reverse@ImageDimensions@img,stringentQ=OptionValue["stringentCheck"]},
-Module[{pts,members,vertices,nearest,vertexset,likelymergers,imagegraph,imggraphweight,imggraphpts,vertexpairs,posVertexMergers,meanVertices,Fn},
+Module[{pts,members,vertices,nearest,vertexset,likelymergers,imagegraph,imggraphweight,imggraphpts,vertexpairs,posVertexMergers,
+meanVertices,Fn},
 pts = ImageValuePositions[MorphologicalTransform[img,{"Fill","SkeletonBranchPoints"}], 1];
 (* finding branch points *)
 members = ParallelMap[Block[{elems},
  elems = Dilation[ReplaceImageValue[ConstantImage[0,Reverse@dim],#->1],1];DeleteCases[Union@Flatten@ImageData[elems*Image[segt]],0|0.]
  ]&,pts]; 
-vertices = Cases[Thread[Round@members-> pts],HoldPattern[pattern:{__}/;Length@pattern >= 2 -> _]];(* finding vertices with 2 or more neighbouring cells *)
+vertices = Cases[Thread[Round@members-> pts],HoldPattern[pattern:{__}/;Length@pattern >= 2 -> _]];
+(* vertices with 2 or more neighbour cells *)
 nearest = Nearest[Reverse[vertices, 2]]; (* nearest func for candidate vertices *)
 Fn = GroupBy[MapAt[Sort,(#-> nearest[#,{All,3}]&/@Values[vertices]),{All,2}],Last->First,#]&;
 Which[Not@stringentQ,
@@ -52,10 +54,12 @@ stringentQ,
 (* a better check is to see the pixels separating the vertices are less than 3 blocks *)
 vertexset = Fn[Identity];
 (* candidates for merging*)
-likelymergers = Cases[Normal[vertexset],PatternSequence[{{__Integer}..}-> i:{__List}/;Length[i]>= 2]];(*defining graph properties of the image *)
+likelymergers = Cases[Normal[vertexset],PatternSequence[{{__Integer}..}-> i:{__List}/;Length[i]>= 2]];
+(*defining graph properties of the image *)
 imagegraph = MorphologicalGraph@MorphologicalTransform[img,{"Fill"}];
 imggraphweight = AssociationThread[(EdgeList[imagegraph]/.UndirectedEdge->List )-> PropertyValue[imagegraph,EdgeWeight]];
-imggraphpts = Nearest@Reverse[Thread[VertexList[imagegraph]-> PropertyValue[imagegraph,VertexCoordinates]],2];(* corresponding nodes on the graph *)
+imggraphpts = Nearest@Reverse[Thread[VertexList[imagegraph]-> PropertyValue[imagegraph,VertexCoordinates]],2];
+(* corresponding nodes on the graph *)
 vertexpairs = Union@*Flatten@*imggraphpts/@(Values[likelymergers]);
 (* find pairs < than 3 edgeweights away, take a mean of vertices and update the association with mean position *)
 posVertexMergers = Position[Thread[Lookup[imggraphweight,vertexpairs]< 3],True];
@@ -67,7 +71,8 @@ KeyMap[Union@*Flatten]@vertexset//Normal]
 ]; 
 
 
-formAndComputeMatrices[vertexCoordinatelookup_,inds_,colsOrder_,edgenum_,delV_,vertexToCells_,vertexvertexConn_,maxcellLabels_,filteredvertices_,vertexAssoc_]:= 
+formAndComputeMatrices[vertexCoordinatelookup_,inds_,colsOrder_,edgenum_,delV_,vertexToCells_,vertexvertexConn_,
+maxcellLabels_,filteredvertices_,vertexAssoc_]:= 
 Module[{tx,ty,tensinds,filteredvertexnum,relabelvert,spArrayTx,spArrayTy,spArrayPx,spArrayPy,spArrayX,spArrayY,$filteredvertices},
 {tx,ty}=Transpose[
 With[{target=vertexCoordinatelookup[#[[2]]],source=vertexCoordinatelookup[#[[1]]]},
@@ -84,7 +89,8 @@ MapThread[(spArrayTx[[Sequence@@#1]]=#2)&,{tensinds,tx}];
 MapThread[(spArrayTy[[Sequence@@#1]]=#2)&,{tensinds,ty}];
 spArrayPx=spArrayPy=SparseArray@ConstantArray[0,{filteredvertexnum,maxcellLabels}];
 MapThread[Print[Style[#1<> "coefficients stats: ",Blue],Counts@Map[Total@*Unitize,Normal[#2]]]&,{{"Tx ", "Ty "},{spArrayTx,spArrayTy}}];
-Print[Style["Tension coefficients dist: ",Bold],Histogram[{{},Join[spArrayTx["NonzeroValues"],spArrayTy["NonzeroValues"]]},20,ImageSize->Small]];
+Print[Style["Tension coefficients dist: ",Bold],
+ Histogram[{{},Join[spArrayTx["NonzeroValues"],spArrayTy["NonzeroValues"]]},20,ImageSize->Small]];
 Block[{neighbouringCells,bisectionlabels,bisectpts,centroid,orderingT,vertexcoords,orderptsT,orderIndT,orderCells,kk=0,px,py},
 With[{cellToVertexLabelsT= Reverse[vertexToCells,2], edgeVertexPart=GroupBy[vertexvertexConn~Flatten~1 ,First-> Last]},
 With[{cellToAllVerticesT= GroupBy[Flatten[Thread/@cellToVertexLabelsT],First-> Last]},
@@ -97,7 +103,8 @@ centroid=Mean@vertexcoords;
 orderingT=Ordering[ArcTan[Last@#-Last@centroid,First@#-First@centroid]&/@vertexcoords];
 orderptsT=vertexcoords[[orderingT]];
 orderIndT=Partition[Lookup[vertexAssoc,Append[orderptsT,First@orderptsT]],2,1];
-orderCells=Last@@@Position[(x\[Function] Map[Intersection[x,#]&,orderIndT])/@(cellToAllVerticesT[#]&/@neighbouringCells),x_/;Length[x]==2];
+orderCells=Last@@@Position[(x\[Function] Map[Intersection[x,#]&,orderIndT])/@(cellToAllVerticesT[#]&/@neighbouringCells),
+ x_/;Length[x]==2];
 neighbouringCells=neighbouringCells[[orderCells]];
 bisectpts=Map[vertexCoordinatelookup,orderIndT,{2}];
 {px,py}=Transpose[{(#[[2,2]]-#[[1,2]])/2,-(#[[2,1]]-#[[1,1]])/2}&/@bisectpts];
@@ -112,7 +119,8 @@ Print["Pressure coefficients computed: ",Style["\[CheckmarkedBox]",20]];
 Print[Style["Pressure coefficients zero: ",Red],kk];
 ];
 MapThread[Print[Style[#1<> "coefficients stats: ",Blue],Counts@Map[Total@*Unitize,Normal[#2]]]&,{{"Px ", "Py "},{spArrayPx,spArrayPy}}];
-Print[Style["pressure coefficients dist: ",Bold],Histogram[{{},Join[spArrayPx["NonzeroValues"],spArrayPy["NonzeroValues"]]},ImageSize->Small]];
+Print[Style["pressure coefficients dist: ",Bold],
+ Histogram[{{},Join[spArrayPx["NonzeroValues"],spArrayPy["NonzeroValues"]]},ImageSize->Small]];
 spArrayX=Join[spArrayTx,spArrayPx,2];
 spArrayY=Join[spArrayTy,spArrayPy,2];
 {spArrayX,spArrayY,Dimensions[spArrayTx],Dimensions[spArrayPx]}
@@ -120,7 +128,8 @@ spArrayY=Join[spArrayTy,spArrayPy,2];
 
 
 (* maximize Log-likelihood function *)
-maximizeLogLikelihood[spArrayX_,spArrayY_,dimTx_,dimPx_]:= Module[{range=10.0^Range[-1.5,1.5,0.1],sol,spA,spg,spB,n,m,spb,\[Tau],SMatrix,Q,R,H,h,logL,\[Mu],p},
+maximizeLogLikelihood[spArrayX_,spArrayY_,dimTx_,dimPx_]:= Module[{range=10.0^Range[-1.5,1.5,0.1],sol,spA,spg,spB,n,m,
+spb,\[Tau],SMatrix,Q,R,H,h,logL,\[Mu],p},
 Print[Style["\nwith maximum likelihood",Bold,18]];
 sol=With[{ls=range},
 (*spA=SparseArray@(Join[spArrayX,spArrayY]);*)
@@ -138,7 +147,8 @@ R=DiagonalMatrix[Sign[Diagonal@R]].R;
 H=R[[;;#,;;#]]&@DimspB;
 \!\(\*OverscriptBox[\(h\), \(\[RightVector]\)]\)=R[[;;#,#+1]]&@DimspB;
 h=R[[#+1,#+1]]&@DimspB;
-logL=-(n-m+1)*Log[h^2]+Total[Log[Diagonal[\[Mu] (spB\[Transpose].spB)]["NonzeroValues"]]]-2*Total[Log[Diagonal[H[[1;;-2,1;;-2]]]["NonzeroValues"]]]),{\[Mu],ls}]
+logL=-(n-m+1)*Log[h^2]+Total[Log[Diagonal[\[Mu] (spB\[Transpose].spB)]["NonzeroValues"]]]-
+ 2*Total[Log[Diagonal[H[[1;;-2,1;;-2]]]["NonzeroValues"]]]),{\[Mu],ls}]
 ]
 ];
 Print[ListPlot[{sol,sol},Joined-> {True,False},PlotStyle->{{Red,Thick},{PointSize[0.02],Black}},AxesStyle->{{Black},{Black}},
@@ -196,10 +206,11 @@ Print@Show[Graphics@Riffle[pressurecolours,poly[[collabels]]],edgeImg]
 (*Mains*)
 
 
-ForceInference[filename_,Imgborder:True|False:True]:=Module[{Img,ImgC,segmentation,maxcellLabels,cellsToVertices,vertexnum,edges,smalledges,
-maxedgeLabels,edgeEndPoints,nearest,nearestedgeEndPoints,edge2pixLabels,pos,oldCoords,vertexAssoc,vertexToCells,filteredvertices,filteredvertexnum,
-relabelvert,edgeLabels,edgenum,spArrayTx,spArrayTy,vertexCoordinatelookup,vertexpairs,vertexvertexConn,inds,edgelabelToVert,delV,vertToedges,edgeImg,
-colsOrder,p,spArrayX,spArrayY,dimTx,dimPx},
+ForceInference[filename_,Imgborder:True|False:True]:=
+Module[{Img,ImgC,segmentation,maxcellLabels,cellsToVertices,vertexnum,edges,smalledges,maxedgeLabels,edgeEndPoints,nearest,
+nearestedgeEndPoints,edge2pixLabels,pos,oldCoords,vertexAssoc,vertexToCells,filteredvertices,filteredvertexnum,relabelvert,
+edgeLabels,edgenum,spArrayTx,spArrayTy,vertexCoordinatelookup,vertexpairs,vertexvertexConn,inds,edgelabelToVert,delV,
+vertToedges,edgeImg,colsOrder,p,spArrayX,spArrayY,dimTx,dimPx},
 
 LaunchKernels[];
 ImgC=Img=Binarize@ColorConvert[Import[filename],"Grayscale"];
@@ -214,7 +225,8 @@ Print["vertices found and associated: ", Style["\[CheckmarkedBox]",20]];
 vertexnum=Length@cellsToVertices;
 
 edges=MorphologicalComponents@ImageFilter[If[#[[3,3]] == 1 && Total[#[[2;;-2,2;;-2]],2] == 3, 1, 0]&,ImgC,2];
-(* associate vertices with all edges. for pixel value 1 edge find two nearest pts. for all edges <3, merge the pts together; make changes to the cellToVertex *)
+(* associate vertices with all edges. for pixel value 1 edge find two nearest pts. for all edges <3, merge the pts together;
+make changes to the cellToVertex *)
 (* edges to be deleted *)
 smalledges=Flatten[Position[Values@ComponentMeasurements[edges,"Count"],1|2]];
 maxedgeLabels=Max@edges;
@@ -247,16 +259,16 @@ nearestedgeEndPoints=Map[Flatten@*nearest,edgeEndPoints,{2}];
 vertexAssoc= AssociationThread[Flatten[Values@cellsToVertices,1],Range@vertexnum];
 vertexToCells=Reverse[MapAt[vertexAssoc[#]&,MapAt[Flatten,cellsToVertices,{All,2}],{All,2}],2];
 filteredvertices=Keys@Select[<|vertexToCells|>,(Length[#]>2&)];
-filteredvertexnum=Length@filteredvertices;(* till above we have isolated all vertices that share three or more edges; we can relabel those filtered vertices
- to be the rows of the matrix *)
- relabelvert=AssociationThread[filteredvertices-> Range[Length@filteredvertices]];(* all edges are relabeled to have a unique identity *)
+filteredvertexnum=Length@filteredvertices;(* till above we have isolated all vertices that share three or more edges; 
+we can relabel those filtered vertices to be the rows of the matrix *)
+ relabelvert=AssociationThread[filteredvertices-> Range[Length@filteredvertices]];(*all edges are relabeled to have a unique identity*)
  edgeLabels=AssociationThread[Range[Length@#]->#]&[nearestedgeEndPoints];
  edgenum=Max[Keys@edgeLabels];
- vertexCoordinatelookup=AssociationMap[Reverse,vertexAssoc];(* given the vertex label \[Rule] get the coordinates from the original lookup *)
+ vertexCoordinatelookup=AssociationMap[Reverse,vertexAssoc];(* given vertex label \[Rule] get coordinates from the original lookup *)
  vertexpairs=Map[vertexAssoc,nearestedgeEndPoints,{2}];
 (* edge coordinates to vertex label. take vertices one by one and find all the edges it is a part of. None should be less than 3 *)
 vertexvertexConn= ParallelTable[
-Cases[vertexpairs,{OrderlessPatternSequence[i,p_]}:> {i,p}],
+ Cases[vertexpairs,{OrderlessPatternSequence[i,p_]}:> {i,p}],
 {i,filteredvertices}];
 
 delV=Cases[vertexvertexConn,{{p_,_},{p_,_}}:> p];
@@ -268,7 +280,8 @@ vertToedges=Normal@AssociationMap[Reverse,edgelabelToVert];
 colsOrder=Flatten[Cases[vertToedges,PatternSequence[{OrderlessPatternSequence@@#}-> p_]:> p,Infinity]&/@inds];
 edgeImg=Graphics[{Thickness[0.005],Line@Lookup[vertexCoordinatelookup,edgelabelToVert[#]]&/@colsOrder}];
 
-{spArrayX,spArrayY,dimTx,dimPx}=formAndComputeMatrices[vertexCoordinatelookup,inds,colsOrder,edgenum,delV,vertexToCells,vertexvertexConn,maxcellLabels,filteredvertices,vertexAssoc];
+{spArrayX,spArrayY,dimTx,dimPx}=formAndComputeMatrices[vertexCoordinatelookup,inds,colsOrder,edgenum,delV,vertexToCells,
+ vertexvertexConn,maxcellLabels,filteredvertices,vertexAssoc];
 p = maximizeLogLikelihood[spArrayX,spArrayY,dimTx,dimPx];
 plotMaps[p,segmentation,edgeImg,maxcellLabels,dimTx,vertexToCells,vertexCoordinatelookup,edgeLabels,Imgborder];
 ];
